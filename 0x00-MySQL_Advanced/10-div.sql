3fdb83e4c6738c6e50a9ecadbc89b2d67b1af075
sabah-hue <sabah.abdelbaset@gmail.com>
2024-08-13 06:08:33 +0300

    output issue

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -2,9 +2,8 @@
 -- that divides (and returns) the first by the second number
 -- or returns 0 if the second number is equal to 0
 DELIMITER $$;
-
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS INT DETERMINISTIC
+RETURNS FLOAT DETERMINISTIC
 BEGIN
     IF b = 0 THEN
         RETURN 0;
