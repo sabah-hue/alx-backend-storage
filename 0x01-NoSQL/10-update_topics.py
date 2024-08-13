385412e43ec18b8652551dfe9cd6c75982364886
sabah-hue <sabah.abdelbaset@gmail.com>
2024-08-14 01:50:26 +0300

    python with mongo

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python3
+""" changes all topics of a school document based on the name """
+
+
+def update_topics(mongo_collection, name, topics):
+    """ update topics """
+    mongo_collection.update_many({name}, {$set: {topics}})
