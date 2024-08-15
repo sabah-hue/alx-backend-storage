360ecd7e3b47609e70ded4049d6a42e74c84ef9f
sabah-hue <sabah.abdelbaset@gmail.com>
2024-08-15 04:10:43 +0300

    web

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Implementing an expiring web cache and tracker """
+import requests
+import redis
+
+
+def get_page(url: str) -> str:
+    """
+    uses the requests module to obtain the HTML content
+    of a particular URL and returns it
+    """
+    key = f"count:{url}"
+    count = redis.Redis().incr(key)
+    cached_key = f"html:{url}"
+    res = redis.Redis().get(cached_key)
+    if res is None:
+        res = requests.get(url).text
+        r.setex(cached_key, 10, res)
+    return res
